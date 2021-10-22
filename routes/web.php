2a34d2e910753a3ac7df932ef25e0544ab211945
YanidaNurNabila <60532297+YanidaNurNabila@users.noreply.github.com>
2021-10-22 09:41:31 +0700

    Menyelesaikan video ke 12 membuat Redesign UI

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,15 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home"
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Yanida Nur Nabila",
         "email" => "193040022.yanida@mail.unpas.ac.id",
         "image" => "yanida.jpeg"
@@ -39,6 +41,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -46,6 +49,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author')
     ]);
 });
@@ -53,6 +57,7 @@ Route::get('/categories/{category:slug}', function (category $category) {
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'post',
         'posts' => $author->posts->load('category', 'author'),
     ]);
 });
